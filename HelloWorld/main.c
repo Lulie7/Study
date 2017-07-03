b344e2478d8f1e3bff817c7dea02e8fe17833283
lulie <juliebbhh@gmail.com>
2017-07-03 23:56:38 +0900

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by 우갱끼 on 2017. 7. 3..
+//  Copyright © 2017년 Lulie. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
