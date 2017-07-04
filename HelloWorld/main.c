cf8ce5616e4e53d1878617831dec06a304efb41d
lulie <juliebbhh@gmail.com>
2017-07-04 09:22:45 +0900

    Hello World Test2

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -2,14 +2,13 @@
 //  main.c
 //  HelloWorld
 //
-//  Created by Lulie on 2017. 7. 3.
+//  Created by Lulie on 2017. 7. 4.
 //  Copyright © 2017년 Lulie. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here.
     printf("Hello, World!\n");
     return 0;
 }
